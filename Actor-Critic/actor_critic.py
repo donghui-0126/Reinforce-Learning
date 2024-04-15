8bd961e3d777c5e1345c06de8368e9b33d1abd6f
donghui Lim <lukehui@naver.com>
2024-04-16 01:50:26 +0900

    update

---
+++ Actor-Critic/actor_critic.py
@@ -0,0 +1,19 @@
+from typing import Any
+import pytorch_lightning as pl
+from pytorch_lightning.utilities.types import STEP_OUTPUT, OptimizerLRScheduler
+
+class ActorCritic(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+    
+    
+    def forward(self):
+        return super().forward()
+    
+    
+    def training_step(self):
+        return super().training_step()
+    
+    
+    def configure_optimizers(self):
+        return super().configure_optimizers()
